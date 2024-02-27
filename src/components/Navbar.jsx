87719200482e5f73a37311078830b465d8d0aae2
Andrew Khadder <andrewkh15@gmail.com>
2024-02-27 00:52:11 -0800

    added layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import LinkButton from "./LinkButton";
 
 const Navbar = () => {
   return (
-    <div className="bg-portfolio-lightgray h-2/5 w-[13%] rounded-xl flex text-portfolio-white">
+    <div className="bg-portfolio-lightgray h-2/5 w-[16%] rounded-xl flex text-portfolio-white drop-shadow-xl">
       <div className="flex flex-col w-full items-center">
         <p className="p-3 -mb-8">andrew khadder</p>
         <div className="flex flex-col h-full w-full items-center justify-evenly">
