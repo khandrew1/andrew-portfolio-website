f04c2ea8c8c5f825ff1a40584229a4bcfc0c3a85
Andrew Khadder <andrewkh15@gmail.com>
2024-02-27 01:20:04 -0800

    added bio

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import Bio from "@/components/Bio";
 
 const Page = () => {
     return (
-        <div className="flex">
+        <div className="flex flex-col">
             <Header />
+            <Bio />
         </div>
     );
 }
