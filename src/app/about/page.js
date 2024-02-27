734351e75cb7eac7ed32ca53492c044234d434d5
Andrew Khadder <andrewkh15@gmail.com>
2024-02-27 01:15:23 -0800

    Added Header component

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+const Page = () => {
+    return (
+        <div className="flex">
+            <Header />
+        </div>
+    );
+}
+
+export default Page;
