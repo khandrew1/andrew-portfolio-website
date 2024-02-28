124e028b0c36908570f0236afc18e7d89fe3f870
Andrew Khadder <andrewkh15@gmail.com>
2024-02-28 13:16:31 -0800

    abstracted Education

--- src/app/education/page.js
+++ src/app/education/page.js
@@ -1,9 +1,37 @@
-import Education from "@/components/Education"
+import Education from "@/components/Education";
 
 const Page = () => {
+  const education = {
+    university: "University of California, Riverside",
+    expectedGrad: "06/25",
+    gpa: "3.86",
+    courses: [
+      "Computer Science I & II",
+      "Intermediate Data Structures & Algorithms",
+      "Software Construction",
+      "Discrete Structures",
+      "Formal Language and Automata",
+      "Machine Organization & Assembly Programming",
+    ],
+    clubs: [
+      {
+        name: "ACM@UCR",
+        roles: [
+          "Full Stack Developer - Cutie & Citrus Hack",
+          "Full Stack Developer - bitByBIT",
+        ],
+      },
+    ],
+  };
   return (
-    <Education />
-  )
-}
+    <Education
+      university={education.university}
+      expectedGrad={education.expectedGrad}
+      gpa={education.gpa}
+      courses={education.courses}
+      clubs={education.clubs}
+    />
+  );
+};
 
 export default Page;
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,39 +1,44 @@
-const Education = () => {
+const Education = ({ university, expectedGrad, gpa, courses, clubs }) => {
   return (
     <div className="flex flex-col text-portfolio-white">
       <div className="h-2/5 w-full bg-portfolio-lightgray rounded-xl drop-shadow-lg">
         <div className="pl-3 pt-3 m-0.5">
-            <p className="text-3xl">University of California, Riverside</p>
-            <p className="text-sm italic">expected graduation: 06/25</p>
-            <p className="text-sm italic">GPA: 3.86</p>
+          <p className="text-3xl">{university}</p>
+          <p className="text-sm italic">expected graduation: {expectedGrad}</p>
+          <p className="text-sm italic">GPA: {gpa}</p>
         </div>
         <div className="flex justify-evenly m-0.5 pl-3">
-            <div className="w-1/2">
-                <p className="flex justify-center items-center underline">Relevant Course Work</p>
-                <ul className="flex flex-col list-disc pl-5 mb-4 gap-0.5">
-                    <li>Computer Science I & II</li>
-                    <li>Intermediate Data Structures & Algorithms</li>
-                    <li>Software Construction</li>
-                    <li>Discrete Structures</li>
-                    <li>Formal Language and Automata</li>
-                    <li>Machine Organization & Aseembly Programming</li>
-                </ul>
-            </div>
-            <div className="w-1/2">
-                <p className="flex justify-center items-center underline">Associated Clubs</p>
-                <ul className="flex flex-col list-disc pl-5 mb-4 gap-0.5">
-                    <li>ACM@UCR
-                        <ul className="list-disc pl-5">
-                            <li>Full Stack Developer - Cutie & Citrus Hack</li>
-                            <li>Full Stack Developer - bitByBIT</li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
+          <div className="w-1/2">
+            <p className="flex justify-center items-center underline">
+              Relevant Course Work
+            </p>
+            <ul className="flex flex-col list-disc pl-5 mb-4 gap-0.5">
+              {courses.map((course, idx) => (
+                <li key={idx}>{course}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="w-1/2">
+            <p className="flex justify-center items-center underline">
+              Associated Clubs
+            </p>
+            <ul className="flex flex-col list-disc pl-5 mb-4 gap-0.5">
+              {clubs.map((club, idx) => (
+                <li key={idx}>
+                  {club.name}
+                  <ul className="list-disc pl-5">
+                    {club.roles.map((role, idx) => (
+                      <li key={idx}>{role}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Education
+export default Education;
