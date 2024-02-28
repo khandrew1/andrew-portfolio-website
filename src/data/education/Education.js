a6826a41ffc880e9c6a88088c234b5426bbd953d
Andrew Khadder <andrewkh15@gmail.com>
2024-02-28 14:01:22 -0800

    education cleanup

---
+++ src/data/education/Education.js
@@ -0,0 +1,22 @@
+export const EDUCATION = {
+    university: "University of California, Riverside",
+    expectedGrad: "06/25",
+    gpa: "3.86",
+    courses: [
+      "Computer Science I & II",
+      "Intermediate Data Structures & Algorithms",
+      "Software Construction",
+      "Discrete Structures",
+      "Formal Language and Automata",
+      "Machine Organization & Assembly Programming",
+    ],
+    clubs: [
+      {
+        name: "ACM@UCR",
+        roles: [
+          "Full Stack Developer - Cutie & Citrus Hack",
+          "Full Stack Developer - bitByBIT",
+        ],
+      },
+    ],
+  };
