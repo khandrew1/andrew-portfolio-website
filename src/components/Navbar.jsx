7507acb1cfd9d8919fb6cbd3b52c82d45e5cf8d9
Andrew Khadder <andrewkh15@gmail.com>
2024-02-28 11:45:11 -0800

    added projects section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import LinkButton from "./LinkButton";
 
 const Navbar = () => {
   return (
-    <div className="bg-portfolio-lightgray h-2/5 w-[16%] rounded-xl flex text-portfolio-white drop-shadow-xl">
-      <div className="flex flex-col w-full items-center">
-        <p className="p-3 -mb-8">andrew khadder</p>
-        <div className="flex flex-col h-full w-full items-center justify-evenly">
+    <div className="border-4 rounded-xl flex flex-col items-center text-portfolio-white bg-portfolio-lightgray">
+      <div className="bg-portfolio-lightgray rounded-xl flex flex-col items-center p-5 w-full">
+        <p className="text-sm w-full mx-4 mb-3">andrew khadder</p>
+        <div className="w-full flex flex-col gap-3 items-center -px-5">
           <LinkButton name="about" href="/about" />
-          <LinkButton name="education" href="" />
-          <LinkButton name="projects" href="" />
-          <LinkButton name="resume" href="" />
+          <LinkButton name="education" href="/about" />
+          <LinkButton name="projects" href="/projects" />
+          <LinkButton name="resume" href="/about" />
         </div>
       </div>
     </div>
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,26 @@
+import { FaFigma, FaGithub } from "react-icons/fa";
+import Link from "next/link";
+
+const ProjectCard = ({ name, list, icons }) => {
+  return (
+    <div className="border-4 rounded-xl flex flex-col text-portfolio-white">
+      <div className="h-2/5 w-full bg-portfolio-lightgray rounded-xl drop-shadow-lg">
+        <p className="text-3xl pl-3 pt-3">{name}</p>
+        <ul className="flex flex-col grow list-disc pl-5 mb-4 m-3 gap-0.5">
+          {list.map((point, idx) => (
+            <li key={idx}>{point}</li>
+          ))}
+        </ul>
+        <div className="flex grow ml-3 mb-3 w-fit gap-3">
+          {icons.map((obj, idx) => (
+            <Link key={idx} href={obj.href}>
+              {obj.icon}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
