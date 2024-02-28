1927f9203cc350e213dd918812559d6a001d837b
Andrew Khadder <andrewkh15@gmail.com>
2024-02-28 13:59:21 -0800

    fix import

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,5 @@
 import About from "@/components/about/About";
-import { PROFILE_INFO } from "@/data/about/about";
+import { PROFILE_INFO } from "@/data/about/About";
 
 const Page = () => {
   return <About name={PROFILE_INFO.name} image={PROFILE_INFO.image} bio={PROFILE_INFO.bio} />;
